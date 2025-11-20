1fe849d42921b3ce90f6a4402b025cf164d58249
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-11-20 17:27:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+
+
+
 # i numeri si fanno cosi
 
 print((2.23 + 2) / 3)
@@ -21,6 +24,60 @@ print(
     aweoifjaew;oijfa;eowj
     """
 )
-x = 12
+
+# la variabili possono contenere qualsiasi cosa
+
+giorgia = "Giorgia Di Virgilio"
+edoardo = "Edoardo Di Virgilio"
+
+# inclusa un altra variabile
+
+nomecorrente = edoardo
+
+print(nomecorrente)
+
+
+# boolean, True or False
+
+print(True)
+print(False)
+
+# comparazioni
+
+x = 80
 y = 13
-print((x + y) * 2)
+y = x
+
+print("x", x)
+print("y", y)
+
+print("x > y", x > y)
+print("x >= y", x >= y)
+print("x == y", x == y)
+
+# if, condizioni, l'if prende True o False come argomento
+
+if x > y:
+    print("x è maggiore di y")
+elif x == y:
+    print("x è uguale a y")
+else:
+    print("x è minore di y")
+
+# while, prende come argomento una condizione
+
+x = 0
+while True:
+    print("ciao", x)
+    x += 1
+
+    if x == 10:
+        break
+
+# lo stesso programma puo essere scritto
+#
+
+x = 0
+while x < 10:
+    print("ciao", x)
+    x += 1
