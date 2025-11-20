f4ff405afac4a7375ce6add70527cea8fccde7d0
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-11-20 16:58:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-ciao
+# i numeri si fanno cosi
+
+print((2.23 + 2)/3)
+
+# le stringhe si fanno o con la singola quota
+
+print('Hello World!')
+
+# o con la doppia
+print("foaiuheifauweh")
+print("asdfad")
+
+print("Hello World!")
+
+# se devi usare gli accapi usi le triple quote
+
+print(
+    """
+    Hello World!
+    asdofijaewoifj
+    aweoifjaew;oijfa;eowj
+    """
+)
