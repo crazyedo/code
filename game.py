f93c28a6c5d95ed4afb7cf12d8043e304f5c4281
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2026-01-29 17:01:35 +0100

    adding variables

--- game.py
+++ game.py
@@ -6,8 +6,8 @@
 from readchar import readchar
 
 # Character position
-x = 2
-y = 1
+hero_x = 2
+hero_y = 1
 
 world_x = 10
 world_y = 10
@@ -19,26 +19,26 @@ while True:
     # Draw grid
     for row in range(world_x):
         for col in range(world_y):
-            if row == y and col == x:
+            if row == hero_y and col == hero_x:
                 print("😊", end=" ")
             else:
                 print("⬜", end=" ")
         print()
 
 
-    print('x', x, 'y', y)
+    print('x', hero_x, 'y', hero_y)
 
     # Get single keypress (no Enter needed!)
     key = readchar()
 
     # Move character
-    if key == "w"  and y >0:
-        y -= 1
-    elif key == "s" and y < world_y - 1:
-        y += 1
-    elif key == "a" and x > 0:
-        x -= 1
-    elif key == "d" and x < world_x - 1:
-        x += 1
+    if key == "w"  and hero_y >0:
+        hero_y -= 1
+    elif key == "s" and hero_y < world_y - 1:
+        hero_y += 1
+    elif key == "a" and hero_x > 0:
+        hero_x -= 1
+    elif key == "d" and hero_x < world_x - 1:
+        hero_x += 1
     elif key == "q":  # quit key
         break
